40c5405dae37c84670386174439a24e94904266f
mfaseehdher <mfaseehdher@gmail.com>
2026-06-17 15:20:41 +0200

    feat:improve setup scripts

--- experiments/control/motor_speed/setup_motor_speed.m
+++ experiments/control/motor_speed/setup_motor_speed.m
@@ -1,5 +1,8 @@
 % setup_motor_speed.m
-% Sets workspace variables for Motor_Model.slx
+% Place in: experiments/control/motor_speed/
+% Model file: Motor_Model.slx
+
+model = 'Motor_Model';
 
 K = 0.01; J = 0.01; b = 0.1; R = 1.0; L = 0.5;
 assignin('base', 'K', K);
@@ -14,12 +17,11 @@ u = 1.0 * ones(size(t));
 assignin('base', 't', t);
 assignin('base', 'u', u);
 
-load_system('Motor_Model');
-set_param('Motor_Model', 'SolverType',        'Fixed-step');
-set_param('Motor_Model', 'Solver',            'ode14x');
-set_param('Motor_Model', 'FixedStep',         '0.001');
-set_param('Motor_Model', 'StopTime',          num2str(t_stop));
-set_param('Motor_Model', 'LoadExternalInput', 'on');
-set_param('Motor_Model', 'ExternalInput',     '[t, u]');
-save_system('Motor_Model');
+set_param(model, 'SolverType',        'Fixed-step');
+set_param(model, 'Solver',            'ode14x');
+set_param(model, 'FixedStep',         '0.001');
+set_param(model, 'StopTime',          num2str(t_stop));
+set_param(model, 'LoadExternalInput', 'on');
+set_param(model, 'ExternalInput',     '[t, u]');
+save_system(model);
 fprintf('  Motor speed workspace ready\n');
