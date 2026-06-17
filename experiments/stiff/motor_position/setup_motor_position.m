62d68ebc0f6d211d46c272182d9cae1766832b7b
mfaseehdher <mfaseehdher@gmail.com>
2026-06-17 15:30:51 +0200

    feat:improve Matlab setup scripts for simulink model

--- experiments/stiff/motor_position/setup_motor_position.m
+++ experiments/stiff/motor_position/setup_motor_position.m
@@ -1,5 +1,8 @@
 % setup_motor_position.m
-% Sets workspace variables for Motor_Pos.slx
+% Place in: experiments/stiff/motor_position/
+% Model file: Motor_Pos.slx
+
+model = 'Motor_Pos';
 
 K = 0.01; J = 0.01; b = 0.1; R = 1.0; L = 0.5;
 assignin('base', 'K', K);
@@ -14,12 +17,11 @@ u = 1.0 * ones(size(t));
 assignin('base', 't', t);
 assignin('base', 'u', u);
 
-load_system('Motor_Pos');
-set_param('Motor_Pos', 'SolverType',        'Fixed-step');
-set_param('Motor_Pos', 'Solver',            'ode14x');
-set_param('Motor_Pos', 'FixedStep',         '0.001');
-set_param('Motor_Pos', 'StopTime',          num2str(t_stop));
-set_param('Motor_Pos', 'LoadExternalInput', 'on');
-set_param('Motor_Pos', 'ExternalInput',     '[t, u]');
-save_system('Motor_Pos');
+set_param(model, 'SolverType',        'Fixed-step');
+set_param(model, 'Solver',            'ode14x');
+set_param(model, 'FixedStep',         '0.001');
+set_param(model, 'StopTime',          num2str(t_stop));
+set_param(model, 'LoadExternalInput', 'on');
+set_param(model, 'ExternalInput',     '[t, u]');
+save_system(model);
 fprintf('  Motor position workspace ready\n');
