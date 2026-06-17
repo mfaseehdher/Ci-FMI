ab7792a19087a63d5b4cb4dcb7e16134bc728182
mfaseehdher <mfaseehdher@gmail.com>
2026-06-17 15:27:36 +0200

    feat:improve Matlab setup scripts for simulink model

--- experiments/nonlinear/ball_beam/setup_ball_beam.m
+++ experiments/nonlinear/ball_beam/setup_ball_beam.m
@@ -1,5 +1,8 @@
 % setup_ball_beam.m
-% Sets workspace variables for ball.slx
+% Place in: experiments/nonlinear/ball_beam/
+% Model file: ballBeam.slx
+
+model = 'ballBeam';
 
 m = 0.111; R = 0.015; g = -9.8;
 L = 1.0; d = 0.03; J = 9.99e-6;
@@ -13,12 +16,11 @@ u = 0.1 * ones(size(t));
 assignin('base', 't', t);
 assignin('base', 'u', u);
 
-load_system('ball');
-set_param('ball', 'SolverType',        'Fixed-step');
-set_param('ball', 'Solver',            'ode14x');
-set_param('ball', 'FixedStep',         '0.001');
-set_param('ball', 'StopTime',          num2str(t_stop));
-set_param('ball', 'LoadExternalInput', 'on');
-set_param('ball', 'ExternalInput',     '[t, u]');
-save_system('ball');
+set_param(model, 'SolverType',        'Fixed-step');
+set_param(model, 'Solver',            'ode14x');
+set_param(model, 'FixedStep',         '0.001');
+set_param(model, 'StopTime',          num2str(t_stop));
+set_param(model, 'LoadExternalInput', 'on');
+set_param(model, 'ExternalInput',     '[t, u]');
+save_system(model);
 fprintf('  Ball beam workspace ready\n');
