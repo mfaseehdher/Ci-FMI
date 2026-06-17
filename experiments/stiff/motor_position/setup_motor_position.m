e48e137607b87526c6d54365b8bb9613caee46e1
mfaseehdher <mfaseehdher@gmail.com>
2026-06-18 00:36:44 +0200

    feat: Improve Matlab setup scripts for simulink model

--- experiments/stiff/motor_position/setup_motor_position.m
+++ experiments/stiff/motor_position/setup_motor_position.m
@@ -1,4 +1,4 @@
-% setup_motor_position.m -- DC motor measuring position
+% setup_motor_position.m
 model = 'Motor_Pos';
 
 K = 0.01;  J = 0.01;  b = 0.1;  R = 1.0;  L = 0.5;
@@ -22,4 +22,17 @@ set_param(model, 'StopTime',          num2str(t_stop));
 set_param(model, 'LoadExternalInput', 'on');
 set_param(model, 'ExternalInput',     '[t, u]');
 save_system(model);
+
+sim(model);
+
+t_out  = tout;
+pos_out = yout{1}.Values.Data(:);
+
+fid = fopen('Motor_Pos_ref.csv', 'w');
+fprintf(fid, 'time,Position\r\n');
+for i = 1:length(t_out)
+    fprintf(fid, '%.10f,%.10f\r\n', t_out(i), pos_out(i));
+end
+fclose(fid);
 fprintf('  Motor position workspace ready\n');
+fprintf('  Reference saved: Motor_Pos_ref.csv (%d rows)\n', length(t_out));
